7bebfbd39d445a34ce0ec858a85e0e0ee0dfaf31
hugostarte <47392672+hugostarte@users.noreply.github.com>
2021-09-22 11:30:22 +0200

    Rectifications de l'indentation

--- pt2_ex4_tp1.c
+++ pt2_ex4_tp1.c
@@ -1,8 +1,8 @@
 #include <stdio.h> 
 int main (void)
 {
-	char car;
-	scanf("%c", &car);
-	printf("Son code ASCII en decimal est : %d \n En octal : %o \n et en hexa : %x \n",car,car,car);
-	return(0);
+  char car;
+  scanf("%c", &car);
+  printf("Son code ASCII en decimal est : %d \n En octal : %o \n et en hexa : %x \n",car,car,car);
+  return(0);
 }
