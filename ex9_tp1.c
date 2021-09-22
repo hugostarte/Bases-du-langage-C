9287e9e3591ec6423ab408e1caee6e9edf47904c
hugostarte <47392672+hugostarte@users.noreply.github.com>
2021-09-22 11:29:47 +0200

    Rectifications de l'indentation

--- ex9_tp1.c
+++ ex9_tp1.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 int main (void)
 {
-	char minuscule;
-	unsigned char a = 'a';
-	unsigned char A = 'A';
-	unsigned int ecart;
-	ecart = A - a;
-	scanf("%c", &minuscule);
-	printf("%c\n", minuscule+ecart);
-
+  char minuscule;
+  unsigned char a = 'a';
+  unsigned char A = 'A';
+  unsigned int ecart;
+  ecart = A - a;
+  scanf("%c", &minuscule);
+  printf("%c\n", minuscule+ecart);
 }
