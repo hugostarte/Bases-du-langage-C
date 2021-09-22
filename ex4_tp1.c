47b62cdf80af173e658153dfe33a8aa624a9d23b
hugostarte <47392672+hugostarte@users.noreply.github.com>
2021-09-22 11:25:30 +0200

    Rectifications de l'indentation

--- ex4_tp1.c
+++ ex4_tp1.c
@@ -1,8 +1,8 @@
 #include <stdio.h> 
 int main (void)
 {
-	unsigned short int codeAscii;
-	scanf("%u", &codeAscii);
-	printf("Le code correspond au caractère : %c \n",codeAscii);
-	return(0);
+  unsigned short int codeAscii;
+  scanf("%u", &codeAscii);
+  printf("Le code correspond au caractère : %c \n",codeAscii);
+  return(0);
 }
