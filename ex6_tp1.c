6b71b749e90d077cbfb1093def057fba154822fd
hugostarte <47392672+hugostarte@users.noreply.github.com>
2021-09-22 11:27:18 +0200

    Rectifications de l'indentation

--- ex6_tp1.c
+++ ex6_tp1.c
@@ -1,9 +1,9 @@
 #include <stdio.h> 
 int main (void)
 {
-	printf("Impression 1 : %c. \n", 7);     // BIP
-	printf("Impression 2 : %c. \n", '7');   // La chaine de caractere '7'.
-	printf("Impression 3 : %d. \n", 7);     // L'entier 7.
-	printf("Impression 4 : %d. \n", '7');   // Le code ASCII correspondant au caractere '7' en décimal.
-	return (0);
+  printf("Impression 1 : %c. \n", 7);     // BIP
+  printf("Impression 2 : %c. \n", '7');   // La chaine de caractere '7'.
+  printf("Impression 3 : %d. \n", 7);     // L'entier 7.
+  printf("Impression 4 : %d. \n", '7');   // Le code ASCII correspondant au caractere '7' en décimal.
+  return (0);
 }
