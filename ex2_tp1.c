4d390ced702ec4774de745e83ec3528b36ee8737
hugostarte <47392672+hugostarte@users.noreply.github.com>
2021-09-22 11:23:53 +0200

    Correction de l'indentation

--- ex2_tp1.c
+++ ex2_tp1.c
@@ -4,15 +4,15 @@ Mon premier programme C
 #include <stdio.h> 
 int main (void)
 {
-printf ("***************************************************************\n");
-printf ("**              Bienvenu au jeu de Mastermind                **\n");
-printf ("**              Les caractères autorisés sont :              **\n"); 
-printf ("**              b : blanc, j : jaune, r : rouge,             **\n"); 
-printf ("**              v : vert, n : noir et g : gris.              **\n"); 
-printf ("**        Chaque combinaison doit avoir 4 caractères:        **\n"); 
-printf ("**        Exemple : rbjg                                     **\n");   
-printf ("**        Vous avez droit à 10 essais.                       **\n"); 
-printf ("**              Bon courage                                  **\n"); 
-printf ("***************************************************************\n");
-return(0);
+  printf ("***************************************************************\n");
+  printf ("**              Bienvenu au jeu de Mastermind                **\n");
+  printf ("**              Les caractères autorisés sont :              **\n"); 
+  printf ("**              b : blanc, j : jaune, r : rouge,             **\n"); 
+  printf ("**              v : vert, n : noir et g : gris.              **\n"); 
+  printf ("**        Chaque combinaison doit avoir 4 caractères:        **\n"); 
+  printf ("**        Exemple : rbjg                                     **\n");   
+  printf ("**        Vous avez droit à 10 essais.                       **\n"); 
+  printf ("**              Bon courage                                  **\n"); 
+  printf ("***************************************************************\n");
+  return(0);
 }
