3492347bb9b246c127f4d828cc0bc3d8b7f6bf3b
hugostarte <47392672+hugostarte@users.noreply.github.com>
2021-09-22 11:28:57 +0200

    Rectifications de l'indentation

--- ex8_tp1.c
+++ ex8_tp1.c
@@ -1,18 +1,18 @@
 #include <stdio.h> 
 int main (void)
 {
-	unsigned char i= 'A'; 
-	int j=1;
-	unsigned short int retourLigne = 10;
-	unsigned short int guillemets = 34;
-	unsigned short int tabHorizontale = 9;
+  unsigned char i= 'A'; 
+  int j=1;
+  unsigned short int retourLigne = 10;
+  unsigned short int guillemets = 34;
+  unsigned short int tabHorizontale = 9;
 
-	printf ("\n \t i=%d \t \"j=%d\" \n", i, j);
-	printf ("\n \t i=%c \t \" \n", i); 
-	// Réécriture sans utiliser de "\"
-	printf("%c %c i=%d %c %cj=%d %c %c", retourLigne,tabHorizontale,i,tabHorizontale,guillemets,j,guillemets,retourLigne );
-	printf ("%c %c i=%c %c %c %c", retourLigne,tabHorizontale,i,tabHorizontale,guillemets,retourLigne); 
-	return(0);
+  printf ("\n \t i=%d \t \"j=%d\" \n", i, j);
+  printf ("\n \t i=%c \t \" \n", i); 
+  // Réécriture sans utiliser de "\"
+  printf("%c %c i=%d %c %cj=%d %c %c", retourLigne,tabHorizontale,i,tabHorizontale,guillemets,j,guillemets,retourLigne );
+  printf ("%c %c i=%c %c %c %c", retourLigne,tabHorizontale,i,tabHorizontale,guillemets,retourLigne); 
+  return(0);
 }
 
 // retour a la ligne ascii -> 10
