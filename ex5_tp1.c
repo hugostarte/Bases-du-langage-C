0c6fd5f63bb75bdedd1e423963fcf88c8870020b
hugostarte <47392672+hugostarte@users.noreply.github.com>
2021-09-22 11:26:08 +0200

    Rectifications de l'indentation

--- ex5_tp1.c
+++ ex5_tp1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main (void)
 {
-	unsigned char c='B';
-	int j=13;
-	unsigned char a='A';
-	printf("c = %x en hexa et son adresse est %p,\n  j = %x en hexa et son adresse est %p,\n et A = %d en decimal et son adresse est %p,\n ",c,&c,j,&j,a,&a);
-	return(0);
+  unsigned char c='B';
+  int j=13;
+  unsigned char a='A';
+  printf("c = %x en hexa et son adresse est %p,\n  j = %x en hexa et son adresse est %p,\n et A = %d en decimal et son adresse est %p,\n ",c,&c,j,&j,a,&a);
+  return(0);
 }
