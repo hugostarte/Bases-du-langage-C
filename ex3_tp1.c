c2cff334935ff1a34adcfbcbd95065675ed4157a
hugostarte <47392672+hugostarte@users.noreply.github.com>
2021-09-22 11:24:29 +0200

    Update ex3_tp1.c

--- ex3_tp1.c
+++ ex3_tp1.c
@@ -1,10 +1,10 @@
 #include <stdio.h> 
 int main (void)
 {
-	int a, b;
-	a=16;
-	b=016;  
-	printf ("Pourquoi la variable a (=%d) a une valeur différente de la variable b (=%d) ? \n", a, b);
-	// Car il interprete 016 comme un entier codé en octal  -> 016 = 14.
-	return(0);
+  int a, b;
+  a=16;
+  b=016;  
+  printf ("Pourquoi la variable a (=%d) a une valeur différente de la variable b (=%d) ? \n", a, b);
+  // Car il interprete 016 comme un entier codé en octal  -> 016 = 14.
+  eturn(0);
 }
